9a2b2cefb0876d0befbeca810f3f83274f59acec
DrMustafaH <mustafaharith@gmail.com>
2021-01-04 15:34:27 -0800

    added comments to assertEqual file

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,6 @@
+// Function that takes an actual input and compare it to an expected output and returns
 const assertEqual = function (actual, expected) {
+  // a ternary operator to check assertion and return strings if passed or failed situations
   actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
