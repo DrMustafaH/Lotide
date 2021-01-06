a3dcf233351723512f074a67641b1d47cf0d5449
DrMustafaH <mustafaharith@gmail.com>
2021-01-05 17:10:08 -0800

    adding longest array logic to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,8 +8,12 @@ const assertEqual = function (actual, expected) {
 const eqArrays = function (arr1, arr2) {
   // a variable created to help in the compare process
   let isItEqual = true;
+  // variable of longerArr created to find the longer array from the 2 provided to be the main one to compare to
+  let longerArr = arr1
+  // condition statement to compare array lengths and decide which one will be in the longerArr variable
+  arr1.length > arr2.length === true ? longerArr = arr1 : longerArr = arr2;
   // loop over the 2 arrays to get their elements
-  for (let i = 0; i < arr1.length; i++) {
+  for (let i = 0; i < longerArr.length; i++) {
     const e1 = arr1[i];
     const e2 = arr2[i];
     // changing the isItEqual variable according to the comparison output (true/false)
@@ -26,3 +30,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
