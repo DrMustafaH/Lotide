f5438016f0173b9eb2b9372ff59b4ca0d37d5739
DrMustafaH <mustafaharith@gmail.com>
2021-01-10 22:47:48 -0800

    create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const head = require('./head');
+const tail = require('./tail');
+const without = require('./without');
+const middle = require('./middle');
+const countOnly = require('./countOnly');
+const letterPostions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const findKey = require('./findKey');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  without: without,
+  middle: middle,
+  countOnly: countOnly,
+  letterPostions: letterPostions,
+  findKeyByValue: findKeyByValue,
+  map: map,
+  takeUntil: takeUntil,
+  findKey: findKey
+};
