96792607068f50ef69817d8929e7c510e9ae585b
DrMustafaH <mustafaharith@gmail.com>
2021-01-10 21:18:15 -0800

    eqArrays refactored with exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,3 @@
-// Function that takes an actual input and compare it to an expected output and returns
-const assertEqual = function (actual, expected) {
-  // a ternary operator to check assertion and return strings if passed or failed situations
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 // function that takes in 2 arrays and compares them to return a boolean results if they are equal/unequal
 const eqArrays = function (arr1, arr2) {
   // a variable created to help in the compare process
@@ -23,11 +17,4 @@ const eqArrays = function (arr1, arr2) {
   return isItEqual;
 };
 
-
-// Test Code
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+module.exports = eqArrays;
