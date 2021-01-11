a2845348cc68a3feb6fdc7ea3ff51add70b2fcc0
DrMustafaH <mustafaharith@gmail.com>
2021-01-10 21:33:52 -0800

    middle refactored with exports

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-// function that takes in 2 arrays and compares them to return a boolean results if they are equal/unequal
-const eqArrays = function (arr1, arr2) {
-  // a variable created to help in the compare process
-  let isItEqual = true;
-  // variable of longerArr created to find the longer array from the 2 provided to be the main one to compare to
-  let longerArr = arr1
-  // condition statement to compare array lengths and decide which one will be in the longerArr variable
-  arr1.length > arr2.length === true ? longerArr = arr1 : longerArr = arr2;
-  // loop over the 2 arrays to get their elements
-  for (let i = 0; i < longerArr.length; i++) {
-    const e1 = arr1[i];
-    const e2 = arr2[i];
-    // changing the isItEqual variable according to the comparison output (true/false)
-    isItEqual = e1 === e2;
-  }
-  // return the isItEqual variable
-  return isItEqual;
-};
-
-// function that takes is 2 arrays and compare then using eqArrays function and output an assertion sentence
-const assertArraysEqual = function (actual, expected) {
-  eqArrays(actual, expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-}
-
 // a function that takes in an array and return the middle-most element(s) of the given array.
 const middle = function (inputArr) {
   // a middleElement array to be repopulated by the function
@@ -47,12 +23,4 @@ const middle = function (inputArr) {
   return middleElement;
 }
 
-
-// Test Code
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
